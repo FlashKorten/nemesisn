eb277f4f3ee61a18869c20da422e297f6a2d6c70
Sebastian Korten <Sebastian.Korten@neuland-bfi.de>
2013-01-26 13:05:12 +0100

    Changed from <div class='value'> to $.data

--- js/driller.js
+++ js/driller.js
@@ -7,15 +7,16 @@ $(document).ready(function() {
 collectParams = function() {
   var query = "";
   $('ul.selector li.entry.selected').each(function() {
-    var p = $(this).parent();
-    var value = $(this).find('div.value').text();
+    var $e = $(this);
+    var value = $e.data('Value');
     if (value) {
-      if (p.hasClass('exclusive')) {
+      var $p = $e.parent();
+      if ($p.hasClass('exclusive')) {
         value = - Math.abs(value);
-      } else if ($(this).hasClass('map')){
+      } else if ($e.hasClass('map')){
         value = Math.abs(value);
       }
-      query += (query === "" ? "" : "&") + p.attr('id') + "=" + value;
+      query += (query === "" ? "" : "&") + $p.attr('id') + "=" + value;
     }
   });
   return query;
@@ -45,174 +46,190 @@ getDrillerJson = function() {
 };
 
 listScenarios = function(data) {
+  var s = data.Scenarios;
+  var title;
   var scenarios = '<ul class="scenario"><li class="numbers">'+data.NoResults+' Scenarios found.<\/li><\/ul>';
   for (var d in data.Scenarios) {
-    scenarios += "<li>"+data.Scenarios[d].Title;
-    if (data.Scenarios[d].Subtitle !== "") {
-      scenarios += "&mdash;"+data.Scenarios[d].Subtitle;
-    }
-    scenarios += "<\/li>";
+    title = s[d].Title + (s[d].Subtitle === "" ? "" : " &mdash; " + s[d].Subtitle);
+    scenarios += "<li>" + title + "<\/li>";
   }
   return scenarios;
 };
 
-getGroupInfo = function(e) {
-  var category = e.parent().attr('id') + "Group/";
-  var idGetter = getGetterForCategoryType('id', e.parent());
-  var valueGetter = getGetterForCategoryType('value', e.parent());
-  var catId = e.find('div.value').text();
-  var g = "";
-  $.getJSON("http://bohr/driller/"+category+catId, collectParams(), function(data) {
+getGroupInfo = function($e) {
+  var category = $e.parent().attr('id') + "Group/";
+  var idGetter = getGetterForCategoryType('id', $e.parent());
+  var valueGetter = getGetterForCategoryType('value', $e.parent());
+  var catId = $e.data('Value');
+  var $ul = $('<ul><\/ul>');
+  $e.empty().append($ul);
+  $.getJSON("http://bohr/driller/" + category + catId, collectParams(), function(data) {
     for (var d in data) {
-      g += getLinkedListEntry('entry' ,idGetter(data[d]), valueGetter(data[d]));
+      $ul.append(getLinkedListEntry('entry' ,idGetter(data[d]), valueGetter(data[d])));
     }
-    e.removeClass("group");
-    e.empty().append("<ul>" + g + "<\/ul>");
+    $e.removeClass("group");
   });
 };
 
 updateList = function(item) {
-  var e = $(item);
-  var list = e.parent();
-  var hidden_siblings;
-  if (e.hasClass('header')) {
-    hidden_siblings = e.siblings('.hidden');
-    if (hidden_siblings.length > 0) {
-      hidden_siblings.removeClass('hidden');
-      list.addClass('overlay');
+  var $e = $(item);
+  var $list = $e.parent();
+  var $hidden_siblings;
+  if ($e.hasClass('header')) {
+    $hidden_siblings = $e.siblings('li.hidden');
+    if ($hidden_siblings.length > 0) {
+      $hidden_siblings.removeClass('hidden');
+      $list.addClass('overlay');
     } else {
-      e.siblings('li.entry').addClass('hidden');
-      list.removeClass('overlay');
+      $e.siblings('li.entry').addClass('hidden');
+      $e.siblings('li.group').addClass('hidden');
+      $list.removeClass('overlay');
     }
-  } else if (e.hasClass('entry')) {
-    if (e.hasClass('selected')){
-      hidden_siblings = e.siblings('.hidden');
-      if (hidden_siblings.length > 0) {
-        e.siblings().removeClass('hidden');
+  } else if ($e.hasClass('entry')) {
+    if ($e.hasClass('selected')){
+      $hidden_siblings = $e.siblings('.hidden');
+      if ($hidden_siblings.length > 0) {
+        $e.siblings().removeClass('hidden');
       } else {
-        e.siblings().addClass('hidden');
+        $e.siblings().addClass('hidden');
       }
     } else {
-      while (!list.hasClass('selector')){
-        list = list.parent();
+      if (!$list.hasClass('selector')){
+        var v = $e.data('Value');
+        $list = $list.parent().parent();
+        $list.empty().append($e);
+        $e.data('Value', v);
       }
-      e.addClass('selected');
-      list.empty().append(e);
-      list.append(getClearFilterLI());
+      $e.addClass('selected');
       getDrillerJson();
     }
-  } else if (e.hasClass('group')) {
-    getGroupInfo(e);
-  } else if (e.hasClass('reset')) {
-    e.siblings('.selected').removeClass('selected');
+  } else if ($e.hasClass('group')) {
+    getGroupInfo($e);
+  } else if ($e.hasClass('reset')) {
+    $e.siblings('.selected').removeClass('selected');
     getDrillerJson();
   }
 };
 
 updateSelector = function(data, f1, f2) {
-  var category = $('#'+f2+'.selector');
-  var selected = $('#'+f2+'.selector li.selected');
-  var idGetter = getGetterForCategoryType('id', category);
-  var valueGetter = getGetterForCategoryType('value', category);
+  var $category = $('#'+f2+'.selector');
+  var $selected = $('#'+f2+'.selector li.selected');
+  var idGetter = getGetterForCategoryType('id', $category);
+  var valueGetter = getGetterForCategoryType('value', $category);
   var dp;
 
-  if (selected.length > 0 && selected.find('div.value').text() !== "") {
+  if ($selected.length > 0) {
     dp = data[f1].Entries[0];
     if (dp.length > 0) {
-      selected.parent().empty().append(getClearFilterLI() + getLinkedListEntry('entry selected', idGetter(dp[0]), valueGetter(dp[0])));
+      $selected.parent()
+        .empty()
+        .append(getClearFilterEntry())
+        .append(getLinkedListEntry('entry selected', idGetter(dp[0]), valueGetter(dp[0])));
     }
     return;
   }
 
-  var g = "";
   var hideExclusion = false;
   var noMatch = false;
   var d;
-  var target = $('#' + f2);
+  var $target = $('#' + f2);
+  var numberOfEntries;
+  $target.empty();
   if (data[f1].Groups !== undefined) {
-    var gr = "";
     var num = 0;
+    var matches;
+    var groupId;
     for (d in data[f1].Groups[0]) {
-      num += data[f1].Groups[0][d].Matches;
-      gr += getLinkedListEntry('group hidden', data[f1].Groups[0][d].GroupId, data[f1].Groups[0][d].GroupId+' ('+data[f1].Groups[0][d].Matches+')');
+      matches = data[f1].Groups[0][d].Matches;
+      groupId = data[f1].Groups[0][d].GroupId;
+      num += matches;
+      $target.append(getLinkedListEntry('group hidden', groupId, groupId + ' (' + matches + ')'));
     }
     if (data[f1].Groups[1].length > 0) {
-      g = prependMatches(data[f1].Groups[1]);
+      prependMatches(data[f1].Groups[1], $target, valueGetter);
     }
-    g += getListEntryWithoutValue('header', 'Filter ('+num+')') + gr;
+    $target.prepend(getListEntryWithoutValue('header', 'Filter (' + num + ')'));
   } else if (data[f1].Entries !== undefined) {
-    if (data[f1].Entries[0].length > 0) {
-      g = getListEntryWithoutValue('header', 'Filter ('+data[f1].Entries[0].length+')');
+    numberOfEntries = data[f1].Entries[0].length;
+    if (numberOfEntries > 0) {
+      $target.append(getListEntryWithoutValue('header', 'Filter (' + numberOfEntries + ')'));
       for (d in data[f1].Entries[0]) {
         dp = data[f1].Entries[0][d];
-        g += getLinkedListEntry('entry hidden', idGetter(dp), valueGetter(dp));
+        $target.append(getLinkedListEntry('entry hidden', idGetter(dp), valueGetter(dp)));
       }
     } else {
       hideExclusion = true;
       noMatch = true;
     }
-    if (data[f1].Entries[1] !== undefined && data[f1].Entries[1].length > 0) {
-      g = prependMatches(data[f1].Entries[1]) + g;
+    if (data[f1].Entries[1].length > 0) {
+      prependMatches(data[f1].Entries[1], $target, valueGetter);
     } else if (noMatch) {
-      g = getListEntryWithoutValue('info', 'Nothing...');
+      $target.append(getListEntryWithoutValue('info', 'Nothing...'));
     }
   }
-  target.empty().append(g);
-  fixExclusion(target, hideExclusion);
+  fixExclusion($target, hideExclusion);
 };
 
-fixExclusion = function(target, hideExclusion) {
-  if (target.siblings('div.exclude').length > 0) {
+fixExclusion = function($target, hideExclusion) {
+  if ($target.siblings('div.exclude').length > 0) {
     if (hideExclusion) {
-      target.removeClass('exclusive');
-      target.siblings('div.exclude').addClass('hidden');
+      $target.removeClass('exclusive');
+      $target.siblings('div.exclude').addClass('hidden');
     } else {
-      target.siblings('div.exclude').removeClass('hidden');
-      if (!target.hasClass('exclusive')) {
-        target.siblings('div.exclude').each(function() { this.innerText = '[-]'; });
+      $target.siblings('div.exclude').removeClass('hidden');
+      if (! $target.hasClass('exclusive')) {
+        $target.siblings('div.exclude').each(function() { this.innerText = '[-]'; });
       }
     }
   }
 };
 
-toggleExclusion = function(e) {
-  var list = $(e).siblings('ul.selector');
-  var valueElement = $(list).find('li.entry.selected div.value');
-  $(valueElement).text(valueElement.text() * -1);
-  if (list.hasClass('exclusive')) {
-    list.removeClass('exclusive');
-    e.innerText = '[-]';
+toggleExclusion = function(item) {
+  var $e = $(item);
+  var $list = $e.siblings('ul.selector');
+  var $valueElement = $list.find('li.entry.selected');
+  var value = $valueElement.data('Value');
+  if (value) {
+    $valueElement.data('Value', value * -1);
+  }
+  if ($list.hasClass('exclusive')) {
+    $list.removeClass('exclusive');
+    $e.text('[-]');
   } else {
-    list.addClass('exclusive');
-    e.innerText = '[+]';
+    $list.addClass('exclusive');
+    $e.text('[+]');
   }
   getDrillerJson();
 };
 
-prependMatches = function(e) {
-  var g = "";
-  for (var d in e) {
-    g += getListEntryWithoutValue('matches', e[d].Name);
+prependMatches = function(data, $element, valueGetter) {
+  for (var d in data) {
+    $element.prepend(getListEntryWithoutValue('matches', valueGetter(data[d])));
   }
-  return g;
-};
-
-getListEntry = function(c, value, text) {
-  return '<li class="'+c+'"><div class="value">'+value+'<\/div><div class="text">'+text+'<\/div><\/li>';
 };
 
 getListEntryWithoutValue = function(c, text) {
-  return '<li class="'+c+'"><div class="text">'+text+'<\/div><\/li>';
+  return $('<li class="'+c+'">'+text+'<\/li>');
+};
+
+getListEntry = function(c, value, text) {
+  var $e = getListEntryWithoutValue(c, text);
+  $e.data('Value', value);
+  return $e;
 };
 
 getLinkedListEntry = function(c, value, text) {
   return getListEntry(c, value, '<a href="#">'+text+'<\/a>');
 };
 
-getGetterForCategoryType = function(t, e) {
-  if (e.hasClass('map')) {
+getClearFilterEntry = function() {
+  return getListEntryWithoutValue('reset hidden', 'Clear filter');
+};
+
+getGetterForCategoryType = function(t, $e) {
+  if ($e.hasClass('map')) {
     return t === "id" ? getId : getName;
-  } else if (e.hasClass('games')) {
+  } else if ($e.hasClass('games')) {
     return t === "id" ? getId : getGameValue;
   } else {
     return getValue;
@@ -233,9 +250,5 @@ getValue = function(data){
 
 getGameValue = function(data){
   return data.Title + (data.Subtitle.length > 0 ? " &mdash; " + data.Subtitle : "");
-};
-
-getClearFilterLI = function() {
-  return "<li class='reset hidden'><div class='value'><\/div><div class='text'>Clear filter<\/div><\/li>";
 };
 
